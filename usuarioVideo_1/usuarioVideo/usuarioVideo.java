fbb49547c2aac668fdf4c8fe968b0b07ed4d74df
denisse <denissejoya52000@gmail.com>
2025-02-26 21:17:37 -0600

    Update usuarioVideo.java

--- usuarioVideo_1/usuarioVideo/usuarioVideo.java
+++ usuarioVideo_1/usuarioVideo/usuarioVideo.java
@@ -8,14 +8,14 @@ import java.util.*;
 
 /**
  *
- * @author Perla Díaz 
+ * @author Denisse Joya 
  */
 public class usuarioVideo {
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        Usuario usuario = new Usuario("Bts", "perdi23556@gmail.com");
-        Video vid = new Video("Dynamite", 60);
+        Usuario usuario = new Usuario("Blackpink", "perdi23556@gmail.com");
+        Video vid = new Video("Pink Venom", 60);
         Comentario coment = new Comentario();
         usuario.verVideo(vid);
         vid.detener();
